f6134c62b7a9edb61b50ee1970e2633a4331c51d
moniad <moni.dziedzic@gmail.com>
2021-11-28 10:42:00 +0100

    Insert dots after the end of every predicate. Update questions for tomorrow's consultation

--- opponentsFieldOfView.pl
+++ opponentsFieldOfView.pl
@@ -1,11 +1,11 @@
-jest_w_polu_widzenia_przeciwnika(gracz) :- gracz_w_prostokacie_11x11_dookola_przeciwnika(gracz, przeciwnik), przeciwnik_patrzy_w_strone_gracza
-gracz_w_prostokacie_11x11_dookola_przeciwnika :- abs(gracz.x - przeciwnik.x) <= 5, abs(gracz.y - przeciwnik.y) <= 5
-przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_lewo, gracz_na_lewo(przeciwnik), przeciwnik.x - gracz.x > abs(gracz.y - przeciwnik.y)
-przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_prawo, gracz_na_prawo(przeciwnik), gracz.x - przeciwnik.x > abs(gracz.y - przeciwnik.y)
-przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_gore, gracz_do_gory(przeciwnik), gracz.y - przeciwnik.y > abs(gracz.x - przeciwnik.x)
-przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_dol, gracz_na_dol(przeciwnik), przeciwnik.y - gracz.y > abs(gracz.x - przeciwnik.x)
+jest_w_polu_widzenia_przeciwnika(gracz) :- gracz_w_prostokacie_11x11_dookola_przeciwnika(gracz, przeciwnik), przeciwnik_patrzy_w_strone_gracza.
+gracz_w_prostokacie_11x11_dookola_przeciwnika :- abs(gracz.x - przeciwnik.x) <= 5, abs(gracz.y - przeciwnik.y) <= 5.
+przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_lewo, gracz_na_lewo(przeciwnik), przeciwnik.x - gracz.x > abs(gracz.y - przeciwnik.y).
+przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_prawo, gracz_na_prawo(przeciwnik), gracz.x - przeciwnik.x > abs(gracz.y - przeciwnik.y).
+przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_gore, gracz_do_gory(przeciwnik), gracz.y - przeciwnik.y > abs(gracz.x - przeciwnik.x).
+przeciwnik_patrzy_w_strone_gracza :- przeciwnik_patrzy_w_dol, gracz_na_dol(przeciwnik), przeciwnik.y - gracz.y > abs(gracz.x - przeciwnik.x).
 
-gracz_na_lewo(przeciwnik) :- przeciwnik.x - gracz.x > 0
-gracz_na_prawo(przeciwnik) :- gracz.x - przeciwnik.x > 0
-gracz_do_gory(przeciwnik) :- gracz.y - przeciwnik.y > 0
-gracz_na_dol(przeciwnik) :- przeciwnik.y - gracz.y > 0
+gracz_na_lewo(przeciwnik) :- przeciwnik.x - gracz.x > 0.
+gracz_na_prawo(przeciwnik) :- gracz.x - przeciwnik.x > 0.
+gracz_do_gory(przeciwnik) :- gracz.y - przeciwnik.y > 0.
+gracz_na_dol(przeciwnik) :- przeciwnik.y - gracz.y > 0.
--- opponentsMotionPattern.pl
+++ opponentsMotionPattern.pl
@@ -1,23 +1,23 @@
 =================== ruchy przeciwnika ===============
 
-popatrz_w_strone(gracz) :- zaniepokojony(przeciwnik)
+popatrz_w_strone(gracz) :- zaniepokojony(przeciwnik).
 
-:- dynamic ma_wartosc/2, zmien_pozycje/3, pozycja/3
+:- dynamic ma_wartosc/2, zmien_pozycje/3, pozycja/3.
 
 #################### OBCHÓD ####################
-zaczyna_obchod(przeciwnik) :- spokojny(przeciwnik)
+zaczyna_obchod(przeciwnik) :- spokojny(przeciwnik).
 # zerowanie licznika kroków, gdy przeciwnik zaczyna obchód lub zrobił pełny obchód
-retract(ma_wartosc(licznik_krokow, _)) :- zaczyna_obchod(przeciwnik)
-retract(ma_wartosc(licznik_krokow, _)) :- ma_wartosc(licznik_krokow, 36)
-ma_wartosc(licznik_krokow, 0) :- zaczyna_obchod(przeciwnik)
-ma_wartosc(licznik_krokow, 0) :- ma_wartosc(licznik_krokow, 36)
+retract(ma_wartosc(licznik_krokow, _)) :- zaczyna_obchod(przeciwnik).
+retract(ma_wartosc(licznik_krokow, _)) :- ma_wartosc(licznik_krokow, 36).
+ma_wartosc(licznik_krokow, 0) :- zaczyna_obchod(przeciwnik).
+ma_wartosc(licznik_krokow, 0) :- ma_wartosc(licznik_krokow, 36).
 
 
 # obchod zaczyna sie od 3 krokow w prawo, a potem w gore
-zrob_krok_w_prawo :- ma_wartosc_z_zakresu(licznik_krokow, [0, 1, 2, 6, 7, 8, 12, 13, 14]), nastapily_dwa_ruchy_gracza
-zrob_krok_w_gore :- ma_wartosc_z_zakresu(licznik_krokow, [3, 4, 5, 27, 28, 29, 33, 34, 35]), nastapily_dwa_ruchy_gracza
-zrob_krok_w_dol :- ma_wartosc_z_zakresu(licznik_krokow, [9, 10, 11, 15, 16, 17, 21, 22, 23]), nastapily_dwa_ruchy_gracza
-zrob_krok_w_lewo :- ma_wartosc_z_zakresu(licznik_krokow, [18, 19, 20, 24, 25, 26, 30, 31, 32]), nastapily_dwa_ruchy_gracza
+zrob_krok_w_prawo :- ma_wartosc_z_zakresu(licznik_krokow, [0, 1, 2, 6, 7, 8, 12, 13, 14]), nastapily_dwa_ruchy_gracza.
+zrob_krok_w_gore :- ma_wartosc_z_zakresu(licznik_krokow, [3, 4, 5, 27, 28, 29, 33, 34, 35]), nastapily_dwa_ruchy_gracza.
+zrob_krok_w_dol :- ma_wartosc_z_zakresu(licznik_krokow, [9, 10, 11, 15, 16, 17, 21, 22, 23]), nastapily_dwa_ruchy_gracza.
+zrob_krok_w_lewo :- ma_wartosc_z_zakresu(licznik_krokow, [18, 19, 20, 24, 25, 26, 30, 31, 32]), nastapily_dwa_ruchy_gracza.
 
 # czy element należy do listy
 ma_wartosc_z_zakresu(licznik_krokow, [licznik_krokow | _]).
@@ -25,34 +25,34 @@ ma_wartosc_z_zakresu(licznik_krokow, [_ | T]) :- ma_wartosc_z_zakresu(licznik_kr
 
 
 # usuniecie z bazy wiedzy potencjalnego poprzedniego kierunku patrzenia (konieczne, gdy nastąpiła zmiana kierunku chodu):
-retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_prawo
-retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_prawo
-retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_prawo
+retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_prawo.
+retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_prawo.
+retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_prawo.
 
-retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_lewo
-retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_lewo
-retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_lewo
+retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_lewo.
+retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_lewo.
+retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_lewo.
 
-retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_gore
-retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_gore
-retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_gore
+retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_gore.
+retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_gore.
+retract(przeciwnik_patrzy_w_dol) :- zrob_krok_w_gore.
 
-retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_dol
-retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_dol
-retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_dol
+retract(przeciwnik_patrzy_w_prawo) :- zrob_krok_w_dol.
+retract(przeciwnik_patrzy_w_lewo) :- zrob_krok_w_dol.
+retract(przeciwnik_patrzy_w_gore) :- zrob_krok_w_dol.
 
 
 # patrzenie w tę samą stronę, w którą odbywają się kroki
-przeciwnik_patrzy_w_prawo :- zrob_krok_w_prawo
-przeciwnik_patrzy_w_lewo :- zrob_krok_w_lewo
-przeciwnik_patrzy_w_gore :- zrob_krok_w_gore
-przeciwnik_patrzy_w_dol :- zrob_krok_w_dol
+przeciwnik_patrzy_w_prawo :- zrob_krok_w_prawo.
+przeciwnik_patrzy_w_lewo :- zrob_krok_w_lewo.
+przeciwnik_patrzy_w_gore :- zrob_krok_w_gore.
+przeciwnik_patrzy_w_dol :- zrob_krok_w_dol.
 
 
-zmien_pozycje(przeciwnik, x + 1, y) :- zrob_krok_w_prawo
-zmien_pozycje(przeciwnik, x, y + 1) :- zrob_krok_w_gore
-zmien_pozycje(przeciwnik, x, y - 1) :- zrob_krok_w_dol
-zmien_pozycje(przeciwnik, x - 1, y) :- zrob_krok_w_lewo
+zmien_pozycje(przeciwnik, x + 1, y) :- zrob_krok_w_prawo.
+zmien_pozycje(przeciwnik, x, y + 1) :- zrob_krok_w_gore.
+zmien_pozycje(przeciwnik, x, y - 1) :- zrob_krok_w_dol.
+zmien_pozycje(przeciwnik, x - 1, y) :- zrob_krok_w_lewo.
 
 
 # usuniecie z bazy wiedzy poprzedniej pozycji przeciwnika, aktualnego kierunku kroku przeciwnika i zmiennej mowiacej, ze przeciwnik moze wykonac kolejny ruch
