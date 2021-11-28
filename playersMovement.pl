348a8e2dc6f7ffabb18877934fa71c496679d6a0
blagosz <lagosz.bartlomiej@gmail.com>
2021-11-28 22:25:17 +0100

    Rewrite players movement to prolog

--- playersMovement.pl
+++ playersMovement.pl
@@ -1,24 +1,17 @@
-=================== ruchy gracza ===============
+%=================== ruchy gracza ===============
 :- dynamic pozycja/3.
+:- dynamic ruchy_gracza/1.
+pozycja(gracz, 0, 0).
+ruchy_gracza(0).
 
-# zrob_krok to event z poziomu Pythona
-zrob_krok_graczem_w_lewo :- zrob_krok(gracz, lewo).
-zrob_krok_graczem_w_prawo :- zrob_krok(gracz, prawo).
-zrob_krok_graczem_w_gore :- zrob_krok(gracz, gora).
-zrob_krok_graczem_w_dol :- zrob_krok(gracz, dol).
+%# zrob_krok to event z poziomu Pythona
+zrob_krok_graczem_w_lewo :- pozycja(gracz, X, Y), S is X - 1, pole_miesci_sie_w_planszy(S, Y), zrob_krok(S, Y).
+zrob_krok_graczem_w_prawo :- pozycja(gracz, X, Y), S is X + 1, pole_miesci_sie_w_planszy(S, Y), zrob_krok(S, Y).
+zrob_krok_graczem_w_gore :- pozycja(gracz, X, Y), S is Y + 1, pole_miesci_sie_w_planszy(X, S), zrob_krok(X, S).
+zrob_krok_graczem_w_dol :- pozycja(gracz, X, Y), S is Y - 1, pole_miesci_sie_w_planszy(X, S), zrob_krok(X, S).
+zrob_krok(X, Y) :- retract(pozycja(gracz, _, _)), asserta(pozycja(gracz, X, Y)), aktualizuj_licznik_ruchow_gracza.
+pole_miesci_sie_w_planszy(X, Y) :- X >= 0, X < 30, Y >= 0, Y < 30.
 
-zmien_pozycje(gracz, X - 1, Y) :- zrob_krok_graczem_w_lewo, pole_miesci_sie_w_planszy(X - 1, Y).
-zmien_pozycje(gracz, X + 1, Y) :- zrob_krok_graczem_w_prawo, pole_miesci_sie_w_planszy(X + 1, Y).
-zmien_pozycje(gracz, X, Y + 1) :- zrob_krok_graczem_w_gore, pole_miesci_sie_w_planszy(X, Y + 1).
-zmien_pozycje(gracz, X, Y - 1) :- zrob_krok_graczem_w_dol, pole_miesci_sie_w_planszy(X, Y - 1).
-
-retract(pozycja(gracz, _, _)) :- zmien_pozycje(gracz, _, _).
-pozycja(gracz, NOWY_X, NOWY_Y) :- zmien_pozycje(gracz, NOWY_X, NOWY_Y).
-
-# update flagi dla kolejnego kroku w obchodzie przeciwnika
-nastapily_dwa_ruchy_gracza :- zmien_pozycje(gracz, _, _), nastapil_jeden_ruch_gracza.
-retract(nastapil_jeden_ruch_gracza) :- nastapily_dwa_ruchy_gracza.
-retract(zmien_pozycje(gracz, _, _)) :- nastapily_dwa_ruchy_gracza.
-
-nastapil_jeden_ruch_gracza :- zmien_pozycje(gracz, _, _).
-retract(zmien_pozycje(gracz, _, _)) :- nastapil_jeden_ruch_gracza.
+%# update flagi dla kolejnego kroku w obchodzie przeciwnika
+aktualizuj_licznik_ruchow_gracza :- ruchy_gracza(0), retract(ruchy_gracza(0)), asserta(ruchy_gracza(1)).
+aktualizuj_licznik_ruchow_gracza :- ruchy_gracza(1), retract(ruchy_gracza(1)), asserta(ruchy_gracza(0)).
