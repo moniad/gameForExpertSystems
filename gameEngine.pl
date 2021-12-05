2f7efc57d354efebe17190b1464bb17a195a31fb
blagosz <lagosz.bartlomiej@gmail.com>
2021-12-05 15:49:47 +0100

    Update movement logic, rewrite some prolog files to English, implement opponents states and winning/loosing conditions

--- gameEngine.pl
+++ gameEngine.pl
@@ -1,31 +1,30 @@
-- system eks. sterujący przeciwnikiem i grą
-- w bazie wiedzy trzymamy fakty o pozycji gracza i przeciwnika (tutaj też info o jego rotacji).
-W razie zmian usuwamy stare reguly i dodajemy nowe
-- ruchy gracza i przeciwnika polegają na każdorazowym przejściu o jedno pole w lewo/prawo/górę/dół
-- przeciwnik ma 2 stany: spokojny i zaniepokojony. W stanie spokojnym robi obchód. Gdy gracz zblizy się zbyt blisko do przeciwnika i nie kuca, przeciwnik go wyczuje/usłyszy i wówczas przeciwnik staje się zaniepokojony. Tym samym obchód kończy się, a przeciwnik się obraca w stronę gracza i z kolejnym ruchem rozpoczyna znowu obchód.
+%- system eks. sterujący przeciwnikiem i grą
+%- w bazie wiedzy trzymamy fakty o pozycji gracza i przeciwnika (tutaj też info o jego rotacji).
+%W razie zmian usuwamy stare reguly i dodajemy nowe
+%- ruchy gracza i przeciwnika polegają na każdorazowym przejściu o jedno pole w lewo/prawo/górę/dół
+%- przeciwnik ma 2 stany: spokojny i zaniepokojony. W stanie spokojnym robi obchód. Gdy gracz zblizy się zbyt blisko do przeciwnika i nie kuca, przeciwnik go wyczuje/usłyszy i wówczas przeciwnik staje się zaniepokojony. Tym samym obchód kończy się, a przeciwnik się obraca w stronę gracza i z kolejnym ruchem rozpoczyna znowu obchód.
 
-- w Prologu mamy programować sprawdzanie, czy gracz nie wychodzi poza planszę, tak jak dla przeciwnika
-- gracz musi wywnioskować algorytm obchodu przeciwnika (np. przeciwnik będzie sie poruszac po kwiatku) - jakiś skomplikowany, ustalony algorytm
-- gracz może kucać max przez 5 ruchów
-- co z kierunkiem patrzenia przeciwnika? na razie będziemy go znać w pełni (później można zmodyfikować grę - tzn. zwiększyć długość kroku przeciwnika, dodać wnioskowanie kierunku patrzenia)
+%- w Prologu mamy programować sprawdzanie, czy gracz nie wychodzi poza planszę, tak jak dla przeciwnika
+%- gracz musi wywnioskować algorytm obchodu przeciwnika (np. przeciwnik będzie sie poruszac po kwiatku) - jakiś skomplikowany, ustalony algorytm
+%- gracz może kucać max przez 5 ruchów
+%- co z kierunkiem patrzenia przeciwnika? na razie będziemy go znać w pełni (później można zmodyfikować grę - tzn. zwiększyć długość kroku przeciwnika, dodać wnioskowanie kierunku patrzenia)
 
-=================== wygrana/przegrana ===============
+%=================== wygrana/przegrana ===============
 
-# gdy przeciwnik widzi gracza, gracz przegrywa
-ginie(gracz) :- odleglosc(gracz, przeciwnik, 1-5), stoi(gracz).
-ginie(gracz) :- jest_w_polu_widzenia_przeciwnika(gracz).
+%# gdy przeciwnik widzi gracza, gracz przegrywa
+dies(player) :- distance(player, opponent, DIST), DIST =< 5, not(crawls(player)).
+ginie(gracz) :- in_opponent_fov(player).
 
-wygrywa(gracz) :- spokojny(przeciwnik), odleglosc(gracz, przeciwnik, 1).
+wygrywa(gracz) :- calm(opponent), distance(player, opponent, DIST), DIST = 1, crawls(player).
 
-
------------------ pytania ------------------
-- w Prologu jest domyślnie backward chaining - czyli apka ma działać tak, że odpytujemy z poziomu aplikacji
-Pythonowej o to, czy po wykonaniu ruchu gracz wygrywa/przegrywa, gdzie przesunąć gracza i przeciwnika?
-- czy da się wywnioskować dwa predykaty, jeśli ich przesłanka złożona z jednego predykatu bedzie prawdziwa?
-np. # patrzenie w tę samą stronę, w którą odbywają się kroki
-chyba wystarczy nie zastosować operatora cięcia? wtedy wszystkie predykaty, które są prawdziwe powinny zostać
-wyprowadzone? chociaż to jest forward chaining...
-- czy początkową pozycję gracza i przeciwnika możemy wstawić poprzez wywołanie Prologa z poziomu Pythona?
-chodzi o to, żeby zapobiec temu, że przy każdym wnioskowaniu będą wstawiane do bazy wiedzy początkowe pozycje gracza i przeciwnika
-- przeciwnik_patrzy_w_dol :- krok_w_dol # TODO: czy "przeciwnik_patrzy_w_dol" z automatu przestanie być prawdziwe, gdyby "krok_w_dol" usunac z bazy wiedzy???
-chyba tak, bo w Prologu nie ma lazy evaluation?
+%----------------- pytania ------------------
+%- w Prologu jest domyślnie backward chaining - czyli apka ma działać tak, że odpytujemy z poziomu aplikacji
+%Pythonowej o to, czy po wykonaniu ruchu gracz wygrywa/przegrywa, gdzie przesunąć gracza i przeciwnika?
+%- czy da się wywnioskować dwa predykaty, jeśli ich przesłanka złożona z jednego predykatu bedzie prawdziwa?
+%np. # patrzenie w tę samą stronę, w którą odbywają się kroki
+%chyba wystarczy nie zastosować operatora cięcia? wtedy wszystkie predykaty, które są prawdziwe powinny zostać
+%wyprowadzone? chociaż to jest forward chaining...
+%- czy początkową pozycję gracza i przeciwnika możemy wstawić poprzez wywołanie Prologa z poziomu Pythona?
+%chodzi o to, żeby zapobiec temu, że przy każdym wnioskowaniu będą wstawiane do bazy wiedzy początkowe pozycje gracza i przeciwnika
+%- przeciwnik_patrzy_w_dol :- krok_w_dol # TODO: czy "przeciwnik_patrzy_w_dol" z automatu przestanie być prawdziwe, gdyby "krok_w_dol" usunac z bazy wiedzy???
+%chyba tak, bo w Prologu nie ma lazy evaluation?
---
+++ movement.pl
@@ -0,0 +1,22 @@
+%=================== ruchy playera ===============
+:- dynamic position/3.
+:- dynamic players_movement_counter/1.
+:- dynamic crawls/1.
+position(player, 0, 0).
+position(opponent, 8, 8).
+players_movement_counter(0).
+
+%# step to event z poziomu Pythona
+step_left :- position(player, X, Y), S is X - 1, step_player(S, Y).
+step_right :- position(player, X, Y), S is X + 1, step_player(S, Y).
+step_up :- position(player, X, Y), S is Y + 1, step_player(X, S).
+step_down :- position(player, X, Y), S is Y - 1, step_player(X, S).
+step_player(X, Y) :- field_on_board(X, Y), retract(position(player, _, _)), asserta(position(player, X, Y)), update_players_movement_counter.
+step_opponent(X, Y) :- players_movement_counter(1), field_on_board(X, Y), retract(position(opponent, _, _)), asserta(position(opponent, X, Y)).
+field_on_board(X, Y) :- X >= 0, X < 30, Y >= 0, Y < 30.
+make_crawl :- asserta(crawls(player)).
+make_stand :- retract(crawls(player)).
+
+%# update flagi dla kolejnego kroku w obchodzie przeciwnika
+update_players_movement_counter :- players_movement_counter(0), retract(players_movement_counter(0)), asserta(players_movement_counter(1)).
+update_players_movement_counter :- players_movement_counter(1), retract(players_movement_counter(1)), asserta(players_movement_counter(0)).
